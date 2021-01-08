688cd5951b22605f6917be3a633c1c750d5227d1
He3k_desktop <cloud.he3k@yandex.ru>
2021-01-08 18:07:44 +0700

    Add new progs

--- install_prog.sh
+++ install_prog.sh
@@ -1,3 +1,22 @@
 #! /bin/bash
 
+# install need progs
 sudo apt install firefox gedit mc tree python3 python git-lfs
+# for install *.deb
+sudo apt install gdebi
+# install ppa
+sudo apt-get install software-properties-common
+# install elementary tweaks
+#sudo add-apt-repository ppa:philip.scott/elementary-tweaks
+#sudo apt update
+#sudo apt install elementary-tweaks
+# install grub custumizer
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo apt update
+sudo apt install grub-customizer
+# install snap
+sudo apt install snapd
+# install spotify
+# sudo apt install spotify telegram-desktop
+
+
